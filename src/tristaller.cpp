4b95e4e02914b98215d7a8d40a4116bb5d798128
thom <thom.v.hout@gmail.com>
2022-09-05 21:53:48 +0200

    display installed mods

--- src/tristaller.cpp
+++ src/tristaller.cpp
@@ -1,32 +1,56 @@
 #include "tristaller.h"
 
-#include "../lib/imgui/imgui.h"
-
+#include <cstdio>
 #include <iostream>
 #include <string>
 #include <vector>
 
+#include <zip.h>
+
+#include "../lib/imgui/imgui.h"
+
 #include "io.h"
+#include "zip_utils.h"
 
-using namespace std;
+using std::string, std::cout, std::endl;
 
-vector<string> mod_selection= {};
+vector<string> mods = {};
+struct bool_wrapper { bool value = false; };
+vector<bool_wrapper> checkboxes = {};
 
 namespace tristaller {
-    bool show_demo_window = false;
+    static bool show_demo_window = false;
 
     const char* mouse_cursors_names[] = { "Arrow", "TextInput", "ResizeAll", "ResizeNS", "ResizeEW", "ResizeNESW", "ResizeNWSE", "Hand", "NotAllowed" };
 
+    static const string mc_dir = (string)getenv("HOME") + "/.minecraft/";
+    static const string installation_dir = mc_dir + "installations/";
+    static const string mod_dir = installation_dir + "/fabric_1.16.5/" + "mods/";
+
+    void initialize() {
+        // Populate installed mods list
+            // TODO Handle dir not found
+        for (auto m : grep_dir(mod_dir, "(.*.jar)", false)) {
+            mods.push_back(m);
+            bool_wrapper bw = {};
+            checkboxes.push_back(bw);
+        }
+        if (readFile()) {
+            cout << "[ERROR]: \tAn error occured while handeling zip file" << endl;
+            throw;
+        }
+
+        return;
+    }
 
     void renderUI() {
-        /* const ImGuiViewport* viewport = ImGui::GetMainViewport();
+        static long unsigned int item_current_idx = 0; // Here we store our selection data as an index.
+
+        const ImGuiViewport* viewport = ImGui::GetMainViewport();
         ImGui::SetNextWindowPos(viewport->WorkPos);
         ImGui::SetNextWindowSize(viewport->WorkSize);
-        */
 
-        ImGui::DockSpaceOverViewport(ImGui::GetMainViewport());
-
-        ImGui::Begin("Main Window");
+        ImGui::Begin("Mods");
 
         if (ImGui::IsItemHovered()) {
             ImGui::SetMouseCursor(2);
@@ -37,48 +61,33 @@ namespace tristaller {
         if (show_demo_window) {
             ImGui::ShowDemoWindow(&show_demo_window);
         }
-        static bool check = false;
-        //ImGui::Checkbox("Checkbox", &check);
 
-        const char* items[] = { "AAAA", "BBBB", "CCCC", "DDDD", "EEEE", "FFFF", "GGGG", "HHHH", "IIII", "JJJJ", "KKKK", "LLLLLLL", "MMMM", "OOOOOOO" };
-        static int item_current_idx = 0; // Here we store our selection data as an index.
-        if (ImGui::BeginListBox("listbox 1", ImVec2(1920, 1080)))
-        {
-            ImGui::Checkbox("Checkbox", &check);
-            if (ImGui::BeginListBox("listbox 2"))
-            {
-                ImGui::Checkbox("Child Checkbox", &check);
-
-                if (ImGui::BeginListBox("listbox 3"))
-                {
-                    ImGui::Checkbox("Child Checkbox", &check);
-                    ImGui::EndListBox();
-                }
-
-                ImGui::EndListBox();
+        //                                     ## => Hide label/name
+        bool lbSuccess = ImGui::ListBoxHeader("##lbMods", ImVec2(-FLT_MIN, mods.size() * ImGui::GetTextLineHeightWithSpacing()));
+        if (lbSuccess) {
+        for (long unsigned int n = 0; n < mods.size(); n++) {
+            string cb_name = "##" + std::to_string(n);
+            if (ImGui::Checkbox(cb_name.c_str(), &checkboxes[n].value)) {
+                // Handle Selection
             }
 
-            for (int n = 0; n < IM_ARRAYSIZE(items); n++)
-            {
-                const bool is_selected = (item_current_idx == n);
-                if (ImGui::Selectable(items[n], is_selected))
-                    item_current_idx = n;
+            ImGui::SameLine(); 
 
-                // Set the initial focus when opening the combo (scrolling + keyboard navigation focus)
-                if (is_selected)
-                    ImGui::SetItemDefaultFocus();
+            string& item = mods[n]; 
+            const bool is_selected = (item_current_idx == n);
+
+            if (ImGui::Selectable(item.c_str(), is_selected)) {
+                // Handle Selection
+                item_current_idx = n;
+                // TODO Display `fabric.mod.properties`
             }
-            ImGui::EndListBox();
-        }
 
-        if (ImGui::Button("List Installed Mods")) {
-            string mc_dir = "~/.minecraft/";
-            string installation_dir = mc_dir + "installations/fabric-1.18.1/";
-            string mod_dir = installation_dir + "mods/";
-//            grep_dir(mod_dir, "*.jar");
-        }
-        if (ImGui::BeginListBox("listbox 1", ImVec2(1920, 1080))) {
-            ImGui::EndListBox();
+            // Set the initial focus when opening the combo (scrolling + keyboard navigation focus)
+            if (is_selected)
+                ImGui::SetItemDefaultFocus();
+            }
+
+            ImGui::ListBoxFooter();
         }
 
         ImGui::End();
@@ -93,6 +102,5 @@ namespace tristaller {
             std::printf("Mac OS X");
         #else
         #endif
-
     }
 }
